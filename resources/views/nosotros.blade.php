bc852154ff295ee150cab694f583a0332d7ad699
yorbelgit <yorbel8perez@gmail.com>
2018-01-29 15:22:33 -0400

    un nuevo parrajo en la vista nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -11,5 +11,12 @@ El 22 de abril de cada año se celebra el nacimiento del movimiento ambientalist
 <p class="lead text-justify" >El Día de la Tierra de 1970 logró una coincidencia política que parecía imposible. Se logró el apoyo de políticos de distintas tendencias, ricos y pobres, citadinos y granjeros, magnates y líderes sindicales. Ese día condujo a la creación de la Agencia de Protección al Medio Ambiente de Estados Unidos y a la aprobación de leyes relacionadas con el aire limpio, el agua limpia y la conservación de especies en peligro de extinción. A partir de entonces, cada año en esta fecha, el mundo entero reflexiona y se moviliza por una Tierra mejor.</p>
 <p class="lead text-justify" >En 2003 decidimos crear un sito web para la recoleccion de ideas que ayuden en la solucion de las distintas problematicas que afectan al medio ambiente y la fuana que alli habitan. Haremos seguimiento a los problemas y esperemos que estas ideas propocionen las mejores soluciones en pro del medio ambiente.</p>
 </div>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
 
 @endsection
